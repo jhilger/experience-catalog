0e3236f94fa79f2c9744d751d37359a839364170
Hilger <c43360@txt.textron.com>
2019-07-15 16:00:52 -0500

    DRY

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 import { getIcon } from "../Icons";
 import "./card.scss";
 
-const Card = props => {
+const Card = ({ sort, experience }) => {
   const [cardSize, setCardSize] = useState(false);
-
   const toggleCard = () => {
     setCardSize(!cardSize);
   };
@@ -14,11 +14,10 @@ const Card = props => {
       className={
         cardSize
           ? `medium-12 medium-order-${Math.floor(
-              props.sort / 2
-            )} large-order-${Math.floor(props.sort / 3)} cell exp-card open`
-          : `medium-6 large-4 medium-order-${Math.floor(props.sort / 2) +
-              1} large-order-${Math.floor(props.sort / 3) +
-              1} cell exp-card close`
+              sort / 2
+            )} large-order-${Math.floor(sort / 3)} cell exp-card open`
+          : `medium-6 large-4 medium-order-${Math.floor(sort / 2) +
+              1} large-order-${Math.floor(sort / 3) + 1} cell exp-card close`
       }
     >
       <button
@@ -32,49 +31,54 @@ const Card = props => {
         className="exp-card-hero"
         style={{
           backgroundImage: `url(${
-            props.experience.Image_URL__c
-              ? props.experience.Image_URL__c
-              : props.experience.default
+            experience.Image_URL__c
+              ? experience.Image_URL__c
+              : "/img/davisestates3.jpg"
           })`
         }}
       >
         <img
-          src={getIcon(props.experience.Experience_Type__c)}
+          src={getIcon(experience.Experience_Type__c)}
           alt="Experience type icon"
         />
       </div>
       <div className="grid-x grid-margin-x grid-margin-y exp-card-main">
         <div className={cardSize ? "medium-6 cell" : "medium-12 cell"}>
           <div className="exp-card-title">
-            <h2>{props.experience.Strategic_Partner__r.Account__r.Name}</h2>
-            <h3>{props.experience.Name}</h3>
+            <h2>{experience.Strategic_Partner__r.Account__r.Name}</h2>
+            <h3>{experience.Name}</h3>
           </div>
           <div
-            className="exp-card-content"
-            dangerouslySetInnerHTML={{ __html: props.experience.content }}
+          // className="exp-card-content"
+          // dangerouslySetInnerHTML={{ __html: experience.content }}
           />
         </div>
         <div className={cardSize ? "medium-6 cell" : "medium-12 cell"}>
           <div className="exp-card-keepinmind">
             <h4>Keep In Mind</h4>
             <div
-              dangerouslySetInnerHTML={{
-                __html: props.experience.Keep_In_Mind__c
-              }}
+            // dangerouslySetInnerHTML={{
+            //   __html: experience.Keep_In_Mind__c
+            // }}
             />
           </div>
           <div className="exp-card-partnerdetails">
             <h5>Partnership Details / Requirements:</h5>
             <div
-              dangerouslySetInnerHTML={{
-                __html: props.experience.partnerdetails
-              }}
+            // dangerouslySetInnerHTML={{
+            //   __html: experience.partnerdetails
+            // }}
             />
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+Card.propTypes = {
+  sort: PropTypes.number.isRequired,
+  experience: PropTypes.object.isRequired
 };
 
 export default Card;
