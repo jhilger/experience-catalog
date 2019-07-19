3d6f76b64fa1a98a77320a90f4ef79d937ec13ee
Craig Couture <couturecraigj@gmail.com>
2019-07-19 08:37:08 -0500

    Updating action creators and input requests

--- src/components/Form/Debug/DebugOutput.js
+++ src/components/Form/Debug/DebugOutput.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-const DebugOutput = ({ values, styles }) => (
-  <div>
-    <pre style={styles}>{JSON.stringify(values, null, 2)}</pre>
-  </div>
-);
+const DebugOutput = ({ values, styles }) => {
+  const [collapsed, setCollapsed] = useState(true);
+  return (
+    <div>
+      <button type="button" onClick={() => setCollapsed(!collapsed)}>
+        Debug Output
+      </button>
+      {!collapsed && (
+        <pre style={styles}>{JSON.stringify(values, null, 2)}</pre>
+      )}
+    </div>
+  );
+};
 
 export default DebugOutput;
--- src/components/Requests/Create/Single.js
+++ src/components/Requests/Create/Single.js
@@ -1,36 +1,48 @@
 import React, { useContext } from "react";
 import Context from "../../Context";
-import Form from "../../Form";
+import Form, { InputField, Debug } from "../../Form";
 import TypeAhead from "../../TypeAhead";
+import { onContactChange, onSubmit } from "./actionCreators";
 
-const SingleRequestCreate = ({ initialValues = {} }) => {
+const SingleRequestCreate = ({ initialValues = {}, onSuccess = () => {} }) => {
   const [{ user, contactId, contacts }, dispatch] = useContext(Context);
   const contact = contacts.data[contactId];
   return (
     <Form
-      onSubmit={console.log}
+      onSubmit={(...args) => dispatch(onSubmit(onSuccess, ...args))}
       initialValues={{
         ...initialValues,
+        Status__c: "Draft",
         Contact_to_Invite__c: contactId,
         Requester__c: user.user_id
       }}
     >
       <TypeAhead
+        required
         name="Contact_to_Invite__c"
         label="Contact"
         sObject="Contact"
-        onChange={record => {
-          dispatch({
-            type: "CONT/data",
-            payload: record
-          });
-          dispatch({
-            type: "CONT/Id",
-            payload: record
-          });
-        }}
+        onChange={record => dispatch(onContactChange(record))}
         value={contact}
       />
+      <InputField
+        component="textarea"
+        label="Customer Restrictions"
+        name="Customer_Restrictions__c"
+      />
+      <InputField
+        required
+        type="date"
+        label="Event Date"
+        name="Event_Date__c"
+      />
+      <InputField required component="select" label="Status" name="Status__c">
+        <option value="Draft">Draft</option>
+        <option value="Submitted">Submitted</option>
+        <option value="Approved">Approved</option>
+        <option value="Rejected">Rejected</option>
+      </InputField>
+      <Debug />
       <button type="submit">Submit</button>
     </Form>
   );
