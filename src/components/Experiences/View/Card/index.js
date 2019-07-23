a0fc02e2b529808d103df95703f7ae78bcf33b5d
Stanfield <c41448@txt.textron.com>
2019-07-23 15:23:34 -0500

    Update card open transition

--- src/components/Experiences/View/Card/index.js
+++ src/components/Experiences/View/Card/index.js
@@ -22,13 +22,20 @@ const Card = ({ sort, experience, expanded = false }) => {
       unmountOnExit
     >
       <div
-        className={
+        /*className={
           cardSize
             ? `medium-12 medium-order-${Math.floor(
                 sort / 2
               )} large-order-${Math.floor(sort / 3)} cell exp-card open`
             : `medium-6 large-4 medium-order-${Math.floor(sort / 2) +
                 1} large-order-${Math.floor(sort / 3) + 1} cell exp-card close`
+        }*/
+        className={
+          cardSize 
+            ? `medium-12 medium-order-${Math.floor( sort / 2
+            )} large-order-${Math.floor(sort / 3)} cell exp-card open`
+            : `medium-6 large-4 medium-order-${Math.floor(sort / 2) +
+            1} large-order-${Math.floor(sort / 3) + 1} cell exp-card close`
         }
       >
         <button
