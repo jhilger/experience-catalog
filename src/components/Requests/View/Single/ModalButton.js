4274cc9cfb96f86b09b861f3e0af3a6fa7931df7
Craig Couture <couturecraigj@gmail.com>
2020-03-05 10:42:31 -0600

    Update Modal

--- src/components/Requests/View/Single/ModalButton.js
+++ src/components/Requests/View/Single/ModalButton.js
@@ -1,18 +1,11 @@
 import React, { useState, useContext } from "react";
 import { withRouter } from "react-router-dom";
 import Modal from "../../../Modal";
-<<<<<<< HEAD
-// import Context from "../../../Context";
-import RequestView from ".";
-=======
-<<<<<<< HEAD:src/components/Requests/View/List/ModalButton.js
+
 import Context from "../../../Context";
 import RequestList from "./Bullet";
-=======
 // import Context from "../../../Context";
 import RequestView from ".";
->>>>>>> 6a891a030dd91f42067ad5ba6b616a39026a4424:src/components/Requests/View/Single/ModalButton.js
->>>>>>> 6a891a030dd91f42067ad5ba6b616a39026a4424
 
 const RequestModalButton = ({ history, id, children }) => {
   const [showReqs, setShowReqs] = useState(false);
